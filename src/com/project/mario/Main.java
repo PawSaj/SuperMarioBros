ec7f24312cec97de53eb2a5e09c1b45618196fbd
sajgon338 <sajgon338@gmail.com>
2017-05-07 23:31:09 +0200

    Initialize project

---
+++ src/com/project/mario/Main.java
@@ -0,0 +1,20 @@
+package com.project.mario;
+
+public class Main implements Runnable {
+
+	public Main() {
+		// TODO Auto-generated constructor stub
+	}
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void run() {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
